f45c4bab5e922c3af058da62f5b54e5be3dfc408
decrypt3723 <hushbeat3723@gmail.com>
2019-10-26 14:55:55 +0900

    mergesort.c added

--- mergesort.c
+++ mergesort.c
@@ -2,29 +2,61 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 
-int readfile(char* , int*);
-void mergesort(int*, int, int);
-void merge(int*, int, int ,int);
-void print_array(int*, int);
+int readfile(char *filename_input, int **arrayptr)
+{
+    FILE *fptr;
+    char filename[50];
+    char stringbuf[50];
+    int i = 0;
+    strcpy(filename, filename_input);
+    if ((fptr = fopen(filename, "r")) == NULL)
+    {
+        printf("This file is not available.");
+        exit(1);
+    }
+    // deciding size of array
+    while ((fscanf(fptr, "%s ", stringbuf)) != EOF)
+    {
+        i++;
+    }
 
-int main (int argc, char* argv[]) {
-	int a[1000];
-	int first = 0;
-	int last;
-	last = readfile(argv[1], a);
-	mergesort(a,first,last);
-	print_array(a,last);
+    // insert elements to memory-allocated array
+    *arrayptr = (int *)malloc((i + 1) * sizeof(int));
+    rewind(fptr);
+
+    i = 0;
+    while ((fscanf(fptr, "%s ", stringbuf)) != EOF)
+    {
+        (*arrayptr)[i++] = atoi(stringbuf);
+    }
+    fclose(fptr);
+    return i - 1;
+}
+void print_array(int *array, int last)
+{
+    int i;
+    printf("\n-----------------------------------------------");
+    for (int i = 0; i <= last; i++)
+    {
+        printf("%d ", array[i]);
+        if ((i + 1) % 10 == 0)
+        {
+            printf("\n");
+        }
+    }
+    printf("\n-----------------------------------------------\n");
 }
 
-void mergesort(int* a, int q, int r) {
-	int middle = ((q+r) / 2);
-	if (q < r) {
-		printf("%d\n",middle);
-		mergesort(a, q, middle);
-		mergesort(a, middle+1, r);
-		merge(a,q,r,middle);
-	}
+void write_file_array(char *output_name, int *array, int last)
+{
+    FILE *fptw = fopen(output_name, "w");
+    for (int i = 0; i <= last; i++)
+    {
+        fprintf(fptw, "%d ", array[i]);
+    }
+    fclose(fptw);
 }
 
 void merge(int* a, int q, int r, int middle) {
@@ -32,25 +64,20 @@ void merge(int* a, int q, int r, int middle) {
 	int j = middle+1;
 	int k = 0;
 	int b[r-q+1];
-	printf("~~\n");
 	while( k < (r-q+1)) {
 		if(i == (middle+1)) {
-			printf("1\n");
 			b[k] = a[j];
 			j++;
 		}
 		else if (j == (r+1)) {
-			printf("2\n");
 			b[k] = a[i];
 			i++;
 		}
 		else if (a[i] <= a[j]) {
-			printf("3\n");
 			b[k] = a[i];
 			i++;
 		}
 		else {
-			printf("4\n");
 			b[k] = a[j];
 			j++;
 		}
@@ -64,32 +91,28 @@ void merge(int* a, int q, int r, int middle) {
 		i++;
 	}
 }
-int readfile(char* filename_input, int* array) {
-	FILE* fptr;
-	char filename[50];
-	char stringbuf[50];
-	int intbuf;
-	int i = 0;
-	printf("5\n");
-	strcpy(filename, filename_input);
-	if((fptr = fopen(filename, "r")) == NULL) {
-		printf("This file is not available.");
-		exit(1);
+
+void mergesort(int* a, int q, int r) {
+	int middle = ((q+r) / 2);
+	if (q < r) {
+		mergesort(a, q, middle);
+		mergesort(a, middle+1, r);
+		merge(a,q,r,middle);
 	}
-	while((fscanf(fptr, "%s ", stringbuf)) != EOF) {
-		intbuf = atoi(stringbuf);
-		array[i++] = intbuf;
-	}
-	fclose(fptr);
-	printf("6\n");
-	return i-1;
 }
-void print_array(int* array, int last) {
-	int i;
-	for(int i = 0; i <= last; i++) {
-		printf("%d ", array[i]);
-		if(i % 10 == 0) {
-			printf("\n");
-		}
-	}
+
+int main (int argc, char* argv[]) {
+	int* a;
+	int** aptr = &a;
+	int first = 0;
+	int last = readfile(argv[1], aptr);
+	clock_t start, end;
+
+	start = clock();
+	mergesort(a,first,last);
+	end = clock();
+
+	printf("%.3lf msec have taken for sorting.", (double)end-start);
+    write_file_array(argv[2], a, last);
+    free(a);
 }
