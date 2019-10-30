d9186f423dcf6544f7aea3a4e234745b6c58318e
decrypt3723 <hushbeat3723@gmail.com>
2019-10-30 17:02:19 +0900

    heapsort to minheap, read char, write char but processing is int

--- heapsort/heapsort_minheap_char.c
+++ heapsort/heapsort_minheap_char.c
@@ -4,27 +4,34 @@
 #include <math.h>
 #include <time.h>
 
-#define dataType char
-#define dataTypeFormat "%c "
+int readfile(char* , int**);
+void print_array(int*, int);
+void swap(int* ,int, int); 
+void heapsort(int** , int* , int**);
+int extract_root(int* , int* );
+void min_heapify(int* , int , int );
+void build_heap(int* , int );
+int find_left_child(int );
+int find_right_child(int );
 
-void heapsort(dataType** a, int* last, dataType** b) {
+void heapsort(int** a, int* last, int** b) {
 	int temp = *last;
 	int i = 0;
 	build_heap(*a, temp);
-	*b = (dataType*) malloc(((*last)+1)*sizeof(dataType));
+	*b = (int*) malloc(((*last)+1)*sizeof(int));
 	while(i <= temp) {
 		(*b)[i] = extract_root(*a, last);
 		i++;
 	}
 }
 
-void build_heap(dataType* a, int last) {
+void build_heap(int* a, int last) {
 	for(int i = ( (last - 1) / 2); i >= 0; i-- ) {
 		min_heapify(a, i, last);
 	}
 }
 
-int extract_root(dataType* a, int* last) {
+int extract_root(int* a, int* last) {
 	int temp;
 	if(*last >= 0) {
 		temp = a[0];
@@ -36,19 +43,19 @@ int extract_root(dataType* a, int* last) {
 	return 0;
 }
 
-void min_heapify(dataType* a, int num, int last) {
-	int smallest = num;
+void min_heapify(int* a, int num, int last) {
+	int least = num;
 	int left_child = find_left_child(num);
 	int right_child = find_right_child(num);
-	if( (left_child <= last) && (a[left_child] < a[smallest]) ) {
-		smallest = left_child;
+	if( (left_child <= last) && (a[left_child] < a[least]) ) {
+		least = left_child;
 	}
-	if( (right_child <= last) && (a[right_child] < a[smallest]) ) {
-		smallest = right_child; 
+	if( (right_child <= last) && (a[right_child] < a[least]) ) {
+		least = right_child; 
 	}
-	if (smallest != num) {
-		swap(a, smallest, num);
-		min_heapify(a, smallest, last);
+	if (least != num) {
+		swap(a, least, num);
+		min_heapify(a, least, last);
 	}
 }
 
@@ -59,67 +66,66 @@ int find_right_child(int num) {
 	return (num*2)+2;
 }
 
-void swap(dataType* a, int input1, int input2) {
+void swap(int* a, int input1, int input2) {
 	int temp;
 	temp = a[input1];
 	a[input1] = a[input2];
 	a[input2] = temp;
 }
 
-int readfile(char* filename_input, dataType** arrayptr) {
+int readfile(char* filename_input, int** arrayptr) {
 	FILE* fptr;
 	char filename[50];
 	char stringbuf[50];
 	int i = 0;
-	dataType buf;
+	char buf;
 	strcpy(filename, filename_input);
 	if((fptr = fopen(filename, "r")) == NULL) {
 		printf("This file is not available.");
 		exit(1);
 	}
 	// deciding size of array
-	while((fscanf(fptr, dataTypeFormat, stringbuf)) != EOF) {
+	while((fscanf(fptr, "%c ", &buf)) != EOF) {
 		i++;
 	}
 	
 	// insert elements to memory-allocated array
-	*arrayptr = (dataType*) malloc((i+1)*sizeof(dataType));
+	*arrayptr = (int*) malloc((i+1)*sizeof(int));
 	rewind(fptr);
 	
 	i = 0;
-	while((fscanf(fptr, dataTypeFormat, &buf)) != EOF) {
-		(*arrayptr)[i++] = buf;
+	while((fscanf(fptr, "%c ", &buf)) != EOF) {
+		(*arrayptr)[i++] = (int)buf;
 	}
 	fclose(fptr);
 	return i-1;
 }
-void print_array(dataType* array, int last) {
+void print_array(int* array, int last) {
 	int i;
 	for(int i = 0; i <= last; i++) {
-		printf(dataTypeFormat, array[i]);
+		printf("%d ", array[i]);
 		if((i+1) % 10 == 0) {
 			printf("\n");
 		}
 	}
 }
 
-void write_file_array(char* output_name, dataType* array, int last) {
+void write_file_array(char* output_name, int* array, int last) {
 	FILE* fptw = fopen(output_name, "w");
 	for(int i = 0; i <= last; i++) {
-		fprintf(fptw, dataTypeFormat, array[i]);
+		fprintf(fptw, "%c ", (char)(array[i]));
 	}
 	fclose(fptw);
 }
 
 int main(int argc, char* argv[]) {
 	clock_t start, end;
-	dataType* a;
-	dataType** aptr = &a;
-	dataType* b;
-	dataType** bptr = &b;
+	int* a;
+	int** aptr = &a;
+	int* b;
+	int** bptr = &b;
 	int last = readfile(argv[1], aptr); // last is heapsize - 1
 	int last_temp = last;
-	print_array(a,last);
 	start = clock();
 	heapsort(aptr, &last, bptr);
 	end = clock();
